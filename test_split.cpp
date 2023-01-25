a7ba209ae9a2e1cf930b5ff69056a375e75d0d72
Jiale Wang <jialewan@usc.edu>
2023-01-25 22:30:45 +0000

    not done yet

--- test_split.cpp
+++ test_split.cpp
@@ -20,7 +20,7 @@ int main(int argc, char* argv[])
     No1two->value = 313;
     head->next = No1two;
     Node* No1three = new Node(0,NULL);
-    No1three->value = 42;
+    No1three->value = 43;
     No1two->next = No1three;
     Node* No1four = new Node(0,NULL);
     No1four->value = 2;
@@ -29,12 +29,11 @@ int main(int argc, char* argv[])
     No1five->value = 1;
     No1four->next = No1five;
     Node* No1six = new Node(0,NULL);
-    No1six->value = 0;
+    No1six->value = 33;
     No1five->next = No1six;
     Node* odds = new Node(0,NULL);
     Node* evens = new Node(0,NULL);
     split(head,odds,evens);
-    cout<<"AAAA"<<endl;
     // while(odds!=NULL)
     // {
     //     cout<<odds->value<<" ";
@@ -50,17 +49,23 @@ int main(int argc, char* argv[])
     Node* next_evens;
     while(evens->next!=NULL)
     {
+      cout<<evens->value<<" ";
       next_evens=evens;
       evens=evens->next;
       delete next_evens;
     }
+    cout<<evens->value<<" ";
     delete evens;
+    cout<<endl;
     Node* next_odds;
     while(odds->next!=NULL)
     {
+      cout<<odds->value<<" ";
       next_odds=odds;
       odds=odds->next;
       delete next_odds;
     }
+    cout<<odds->value<<" ";
+    cout<<endl;
     delete odds;
 }
